6d094b2e9f235807f7c512ce068dad2bfaa09ec3
Ravirbikant <ravirbikant@gmail.com>
2023-01-08 08:55:55 -0800

    1st commit

---
+++ game.js
@@ -0,0 +1,77 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userChosenColor;
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+function startOver(){
+    level=0;
+    gamePattern=[];
+    userClickedPattern=[];
+    started=false;
+}
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel]==gamePattern[currentLevel]){
+        if(userClickedPattern.length==gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    } else{
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+          }, 200);
+    
+          startOver();
+    }   
+}
+
+$(document).keypress(function(){
+    if(!started){
+        $("level-title").text("Level "+level);
+        nextSequence();
+        started=true;
+    }
+});
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(200).fadeIn(200);
+    playSound(randomChosenColour);
+    level++;
+    $("#level-title").html("Level "+level);
+    userClickedPattern=[];
+}
+
+$(".btn").click(function(){
+    userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    },100);
+}
+
+
+
+/* 
+Prepare fresh sequence on every reload or game over with no cache
+*/
